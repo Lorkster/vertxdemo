518efe1394f40296771f5d6d811bfcbc647363fe
Magnus de Pourbaix <magnus.dep+git@gmail.com>
2016-01-29 08:23:45 +0100

    First commit

---
+++ src/main/java/app/Server.java
@@ -0,0 +1,29 @@
+package app;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import util.Runner;
+
+public class Server extends AbstractVerticle {
+
+    public static void main(String... args) {
+        Runner.runClusteredExample(Server.class);
+    }
+
+    @Override
+    public void start(Future<Void> fut) {
+        vertx
+                .createHttpServer()
+                .requestHandler(r -> {
+                    r.response().end("<h1>Hello from my first " +
+                            "Vert.x 3 application</h1>");
+                })
+                .listen(8080, result -> {
+                    if (result.succeeded()) {
+                        fut.complete();
+                    } else {
+                        fut.fail(result.cause());
+                    }
+                });
+    }
+}

8b2c596d7f4e9b2bed712590bd28ae6d72038a30
Jonathan Nisell <jonathan.nisell@find-out.se>
2016-02-02 11:28:20 +0100

    Added Groovy listener and server classes to clustered demo

---
+++ news-server-groovy/src/main/groovy/app/NewsServer.groovy
@@ -0,0 +1,27 @@
+package main.groovy.app
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+import io.vertx.core.eventbus.EventBus
+import main.groovy.util.Runner
+
+/**
+ * Created by jonathan on 2016-01-29.
+ */
+
+class NewsServer extends AbstractVerticle{
+
+    public static void main (String... args){
+        Runner.runClusteredExample(NewsServer.class)
+    }
+
+    @Override
+    public void start(Future<Void> future){
+        println "starting server"
+        EventBus eb = vertx.eventBus()
+        // Send a message every second
+        vertx.setPeriodic(1000, {
+            eb.publish("news-feed", "Some news about Groovy!")
+        })
+    }
+}

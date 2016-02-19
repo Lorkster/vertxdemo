1404c4f4fc9835237e686688413174b37b8bb1b2
Magnus de Pourbaix <magnus.de.pourbaix@find-out.se>
2016-02-19 14:09:59 +0100

    --env-file usage for Docker demonstrated as well as --link used to connect db-service-java to the vertxdemo-mysql container.

--- db-service-java/src/main/java/app/DemoDatabaseServiceImpl.java
+++ db-service-java/src/main/java/app/DemoDatabaseServiceImpl.java
@@ -5,16 +5,72 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.MySQLClient;
+import io.vertx.ext.sql.SQLConnection;
 
 public class DemoDatabaseServiceImpl implements DemoDatabaseService {
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+    private AsyncSQLClient client;
+
     public DemoDatabaseServiceImpl(Vertx vertx) {
+        final String user = System.getenv("MYSQL_USER");
+        final String password = System.getenv("MYSQL_PASSWORD");
+        final String database = System.getenv("MYSQL_DATABASE");
+        final String db_port = System.getenv("DB_PORT");
+        final String db_port_num = db_port.split(":")[2];
+        final String tcpAddr = System.getenv("DB_PORT_"+db_port_num+"_TCP_ADDR");
+
+        log.info("Trying MYSQL_USER {0} and MYSQL_PASSWORD {1} for MYSQL_DATABASE {2}, DB_PORT_{3}_TCP_ADDR {4}",
+                user, password, database, db_port_num, tcpAddr);
+
+        JsonObject mySQLClientConfig = new JsonObject()
+                .put("host", tcpAddr)
+                .put("username", user)
+                .put("password", password)
+                .put("database", database
+                );
+        client = MySQLClient.createShared(vertx, mySQLClientConfig, "MySQLPool1");
+
+        ;
     }
 
     @Override
     public void save(String collection, JsonObject document, Handler<AsyncResult<Void>> resultHandler) {
         System.out.println("Service got: " + document.encodePrettily());
-        if (resultHandler != null) {
-            resultHandler.handle(Future.succeededFuture());
-        }
+
+        client.getConnection(conn -> {
+            if (conn.failed()) {
+                log.error(conn.cause().getMessage());
+                return;
+            }
+
+            SQLConnection connection = conn.result();
+
+            // Got a connection
+            connection.execute("insert into demodata values (NOW(), '"+document.encodePrettily()+"')", insert -> {
+                if (insert.failed()) {
+                    log.error("SELECT failed due to {0}", insert.cause());
+                    resultHandler.handle(Future.failedFuture(insert.cause()));
+                }
+                else {
+                    resultHandler.handle(Future.succeededFuture());
+                }
+
+                connection.close(done -> {
+                    if (done.failed()) {
+                        throw new RuntimeException(done.cause());
+                    }
+                });
+            });
+
+        });
+    }
+
+    @Override
+    public void close() {
+        client.close();
     }
 }
